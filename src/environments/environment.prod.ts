db08b2fdda02aa0b9121b8b78aed53d01a8e1be7
SAURAVBORAH22 <sauravborah1999@gmail.com>
2025-02-25 19:06:14 +0530

    added prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB8r3Hjnx3zLkGqddD8ZqjJz6V5Was_wos",
+    authDomain: "ngrx-learn-a1e2b.firebaseapp.com",
+    projectId: "ngrx-learn-a1e2b",
+    storageBucket: "ngrx-learn-a1e2b.firebasestorage.app",
+    messagingSenderId: "956153872728",
+    appId: "1:956153872728:web:12e4dafb3a15b3df9ca52d",
+    measurementId: "G-NP7F5R6GMQ"
+  }
 };
